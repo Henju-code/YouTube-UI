1b97743b3a6d0dc33f38b251af6a8fb1f319f4df
Henju-Code <henjucode@gmail.com>
2021-06-16 10:23:20 -0300

    chore: initial commit

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+import Home from './pages/Home'
+
+function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  const theme = createMuiTheme({
+    spacing: 4,
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+      primary: {
+        main: '#f44336'
+      },
+      secondary: {
+        main: '#3EA6FF'
+      },
+      background: {
+        default: darkMode ? '#232323': '#fff',
+        dark: darkMode ? '#181818' : '#f4f6f8',
+        paper: darkMode ? '#232323': '#fff'
+      }
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme} >
+      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
+    </ThemeProvider>
+  );
+}
+
+export default App;
